48171bb7131b11ead0d99c42e582258d90f1c0ac
Juan Carlos <gurquiza8@gmail.com>
2024-07-27 04:55:21 -0600

    feature: service done for login to user

---
+++ src/app/auth/interfaces/auth-status.enum.ts
@@ -0,0 +1,10 @@
+
+// Por defecto: [0]: checking, [1]: authenticated, [2]: notAuthenticated
+export enum AuthStatus {
+  checking = 'checking',
+  authenticated = 'authenticated',
+  notAuthenticated = 'notAuthenticated',
+}
+
+
+
