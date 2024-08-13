22623f253a96f42de9f0d9880fc8ad6cb65635e1
Juan Carlos <gurquiza8@gmail.com>
2024-08-13 15:43:15 -0600

    feature: fix table products input

--- src/app/dashboard/modules/work-order/pages/create-work-order/create-work-order.component.ts
+++ src/app/dashboard/modules/work-order/pages/create-work-order/create-work-order.component.ts
@@ -11,47 +11,19 @@ import { FormArray, FormBuilder, FormGroup } from '@angular/forms';
 export class CreateWorkOrderComponent implements OnInit {
 
   public products: Product[] = [];
-  public productForm: FormGroup;
 
   public httpProduct = inject(ProductsService);
-  private fb = inject(FormBuilder);
-
-  constructor() {
-    this.productForm = this.fb.group({
-      products: this.fb.array([])
-    });
-  }
 
   ngOnInit(): void {
     this.loadProducts();
-  }
-
-  get productsFormArray(): FormArray {
-    return this.productForm.get('products') as FormArray;
-  }
-
-  private createProductFormGroup(product: Product): FormGroup {
-    return this.fb.group({
-      id: [product.id],
-      codigoSKU: [product.codigoSKU],
-      family: [product.productFamily.name],
-      name: [product.name],
-      quantity: [''],
-    });
   }
 
   public loadProducts() {
     this.httpProduct.getAllProducts('/getAll')
       .subscribe( (resp) => {
         this.products = resp.allProducts;
-        this.products.forEach( product => {
-          this.productsFormArray.push(this.createProductFormGroup(product));
-        });
+        console.log(this.products);
       });
-  }
-
-  public getFormData() {
-    console.log(this.productForm.value);
   }
 
 }
