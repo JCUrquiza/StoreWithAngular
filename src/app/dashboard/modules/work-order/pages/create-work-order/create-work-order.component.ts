5661ff8338d71e1739adaec11dd56b29b328fba3
Juan Carlos <gurquiza8@gmail.com>
2024-08-17 06:54:57 -0600

    feature: save workOrder service

--- src/app/dashboard/modules/work-order/pages/create-work-order/create-work-order.component.ts
+++ src/app/dashboard/modules/work-order/pages/create-work-order/create-work-order.component.ts
@@ -1,8 +1,11 @@
 import { Component, inject, OnInit } from '@angular/core';
 import { FormArray, FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { MessageService } from 'primeng/api';
+import { tap } from 'rxjs';
 import { ProductsService } from '../../../warehouses/services/products.service';
 import { Product } from '../../../warehouses/interfaces';
 import { ValidatorsService } from '../../../../../shared/service/validators.service';
+import { WorkOrderService } from '../../services/work-order.service';
 
 @Component({
   selector: 'app-create-work-order',
@@ -17,6 +20,8 @@ export class CreateWorkOrderComponent implements OnInit {
   public httpProduct = inject(ProductsService);
   public fb = inject(FormBuilder);
   public validatorsService = inject(ValidatorsService);
+  public workOrderService = inject(WorkOrderService);
+  public messageService = inject(MessageService);
 
   public myForm: FormGroup = this.fb.group({
     address: ['', [
@@ -35,7 +40,7 @@ export class CreateWorkOrderComponent implements OnInit {
       Validators.required,
       Validators.pattern(this.validatorsService.numericPattern)
     ]],
-    productsOfWorkOrder: this.fb.array([], [Validators.required, Validators.minLength(1)]),
+    productsOfWorkOrder: this.fb.array([], [Validators.required]),
   });
 
   ngOnInit(): void {
@@ -73,7 +78,7 @@ export class CreateWorkOrderComponent implements OnInit {
         name: [product.name, Validators.required],
         codigoSKU: [product.codigoSKU, Validators.required],
         familyname: [product.productFamily.name, Validators.required],
-        quantity: [null, [
+        quantity: [0, [
           Validators.required,
           Validators.min(1),
           Validators.pattern(this.validatorsService.numericPattern)
@@ -88,21 +93,48 @@ export class CreateWorkOrderComponent implements OnInit {
     }
   }
 
-
   public removeProduct(index: number): void {
     this.productsFormArray.removeAt(index);
   }
 
-
-
   public sendForm() {
-
     if ( this.myForm.invalid ) {
       this.myForm.markAllAsTouched();
       return;
     }
 
-    console.log(this.myForm.value);
+    // Array para almacenar los productos formateados para enviar
+    const productsToSend: { id: number, quantity: number }[] = [];
+
+    const productsArray = this.myForm.get('productsOfWorkOrder')?.getRawValue();
+    productsArray.forEach((product: any) => {
+      productsToSend.push({
+        id: product.id,
+        quantity: +product.quantity
+      });
+    });
+
+    const body = {
+      address: this.myForm.value.address,
+      priceOfLabor: +this.myForm.value.priceOfLabor,
+      priceOfTransfer: +this.myForm.value.priceOfTransfer,
+      priceTotal: +this.myForm.value.priceTotal,
+      customerId: 11,
+      products: productsToSend
+    }
+    this.workOrderService.create('/create', body).pipe(
+      tap({
+        next: (resp) => {
+          console.log(resp);
+          this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Work Order created successfully' });
+        },
+        error: (error) => {
+          console.log(error);
+          this.messageService.add({ severity: 'error', summary: 'Error', detail: error.error.error });
+        }
+      })
+    ).subscribe();
+
   }
 
 }
